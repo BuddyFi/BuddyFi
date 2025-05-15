f15e0b0cc9ea61a140a176bd23eccc672004add6
jaydattkaran <karanjaydatt03@gmail.com>
2025-05-15 11:55:38 +0530

    added logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import ConnectWalletButton from "./solana/ConnectWalletButton";
 import Link from "next/link";
 
 const Navbar: React.FC = () => (
-    <nav className="absolute left-0 w-[98vw] flex justify-between items-center px-4 top-2">
-        <Link href="/" className="text-xl font-bold">BuddyFi</Link>
-        <ConnectWalletButton/>
-    </nav>
-)
+  <nav className="absolute left-0 w-[98vw] flex justify-between items-center px-4 top-2">
+    <Link href="/">
+      <div className="flex gap-1 items-center">
+        <img
+          src="./BuddyFi_logo.svg"
+          alt="BuddyFi logo"
+          className="md:h-15 md:w-15 h-10 w-10"
+        />
+        <p className="md:text-2xl text-xl font-bold">BuddyFi</p>
+      </div>
+    </Link>
+    <ConnectWalletButton />
+  </nav>
+);
 
 export default Navbar;
