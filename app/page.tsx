1283bfc336c6c09a6773d785b0b02a9c463d34e9
jaydattkaran <karanjaydatt03@gmail.com>
2025-05-23 04:29:30 +0530

    branch test

--- app/page.tsx
+++ app/page.tsx
@@ -227,7 +227,7 @@ const workspace = await buddyfi.createTeam({
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white">
+    <div className="min-h-screen ">
       <Navbar />
 
       {/* hero section */}
