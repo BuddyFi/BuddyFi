d36ebdfe22e9abc44778e81e44946244ee2981f8
jaydattkaran <karanjaydatt03@gmail.com>
2025-07-02 23:42:59 +0530

    auth access modified

--- app/api/github/login/route.ts
+++ app/api/github/login/route.ts
@@ -4,10 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const githubClientId = process.env.GITHUB_CLIENT_ID;
   
-  // Set the scope based on what permissions you need
-  const scope = "read:user user:email read:public_repo repo:status read:org";
+  const scope = "read:user user:email read:org repo:status";
+
   
-  // Redirect to GitHub's OAuth page
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=${scope}`;
   
   return NextResponse.redirect(githubAuthUrl);
