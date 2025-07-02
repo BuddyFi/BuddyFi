2d92fbb472c91f6aec3d894564ffaa182d311ec1
jaydattkaran <karanjaydatt03@gmail.com>
2025-07-02 23:34:19 +0530

    github auth access

--- app/api/github/login/route.ts
+++ app/api/github/login/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const githubClientId = process.env.GITHUB_CLIENT_ID;
   
   // Set the scope based on what permissions you need
-  const scope = "read:user user:email public_repo repo:status read:org";
+  const scope = "read:user user:email read:public_repo repo:status read:org";
   
   // Redirect to GitHub's OAuth page
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=${scope}`;
